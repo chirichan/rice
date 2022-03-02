e1d11ecf81a9e41fedc2bfcfeecee44f37160dd5
woxingliu <57004799+woxingliu@users.noreply.github.com>
2022-03-02 21:35:34 +0800

    stash

--- crypto.go
+++ crypto.go
@@ -5,6 +5,7 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"io"
 
@@ -114,10 +115,74 @@ func AESNewGCMDecrypt(keyString, nonceString, ciphertext string) (string, error)
 	return ByteString(plaintext), nil
 }
 
-func AESNewCBCDecrypter() {}
+func AESNewCBCDecrypter(keyString, cipherString string) (string, error) {
+	key, _ := hex.DecodeString(keyString)
+	ciphertext, _ := hex.DecodeString(cipherString)
 
-func AESNewCBCEncrypter() {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
 
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	if len(ciphertext) < aes.BlockSize {
+		return "", errors.New("ciphertext too short")
+	}
+
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	// CBC mode always works in whole blocks.
+	if len(ciphertext)%aes.BlockSize != 0 {
+		return "", errors.New("ciphertext is not a multiple of the block size")
+	}
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+
+	// CryptBlocks can work in-place if the two arguments are the same.
+	mode.CryptBlocks(ciphertext, ciphertext)
+
+	fmt.Printf("%s\n", ciphertext)
+
+	return ByteString(ciphertext), nil
+}
+
+func AESNewCBCEncrypter(keyString, plainString string) (string, error) {
+	key, _ := hex.DecodeString(keyString)
+	plaintext := []byte(plainString)
+
+	// CBC mode works on blocks so plaintexts may need to be padded to the
+	// next whole block. For an example of such padding, see
+	// https://tools.ietf.org/html/rfc5246#section-6.2.3.2. Here we'll
+	// assume that the plaintext is already of the correct length.
+	if len(plaintext)%aes.BlockSize != 0 {
+		panic("plaintext is not a multiple of the block size")
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
+
+	// It's important to remember that ciphertexts must be authenticated
+	// (i.e. by using crypto/hmac) as well as being encrypted in order to
+	// be secure.
+
+	fmt.Printf("%x\n", ciphertext)
+
+	return hex.EncodeToString(ciphertext), nil
 }
 
 func AESNewCFBDecrypter() {}
