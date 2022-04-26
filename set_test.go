42a13617df30baed5af3c250cab1fc0832e403c2
woxingliu <57004799+woxingliu@users.noreply.github.com>
2022-04-26 18:38:31 +0800

    update

--- set_test.go
+++ set_test.go
@@ -1,6 +1,9 @@
 package rice
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSliceIn(t *testing.T) {
 	type args struct {
@@ -19,6 +22,28 @@ func TestSliceIn(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := SliceIn(tt.args.e, tt.args.s); got != tt.want {
 				t.Errorf("SliceIn() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSliceSet(t *testing.T) {
+	type args struct {
+		s1 []int64
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int64
+	}{
+		// TODO: Add test cases.
+		{name: "1", args: args{[]int64{2, 3, 5, 4, 3, 0, 0, 0, 2, 1}}, want: []int64{2, 3, 5, 4, 0, 1}},
+		{name: "2", args: args{[]int64{0, 0, 0, 0, 0, 0}}, want: []int64{0}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SliceSet(tt.args.s1); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("SliceSet() = %v, want %v", got, tt.want)
 			}
 		})
 	}
