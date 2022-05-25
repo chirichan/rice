c1953bc516cfef3fd01f75e74d6d8714ad64974d
woxingliu <57004799+woxingliu@users.noreply.github.com>
2022-05-25 19:21:36 +0800

    set.go

--- set_test.go
+++ set_test.go
@@ -42,7 +42,7 @@ func TestSliceSet(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := SliceSet(tt.args.s1); !reflect.DeepEqual(got, tt.want) {
+			if got := RemoveDuplicates(tt.args.s1); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("SliceSet() = %v, want %v", got, tt.want)
 			}
 		})
