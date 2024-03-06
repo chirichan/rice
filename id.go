b08900716027b26691dd3c23ba3a6b7334e367d3
chirichan <57004799+chirichan@users.noreply.github.com>
2024-03-06 20:28:31 +0800

    Update .

--- id.go
+++ id.go
@@ -10,15 +10,15 @@ import (
 	"github.com/yitter/idgenerator-go/idgen"
 )
 
-func InitID(workID uint16) {
+func InitYitterID(workID uint16) {
 	options := idgen.NewIdGeneratorOptions(workID)
 	options.BaseTime = time.Date(2022, time.January, 1, 0, 0, 0, 0, time.Local).UnixMilli()
 	idgen.SetIdGenerator(options)
 }
 
-func NextID() int64 { return idgen.NextId() }
+func NextYitterID() int64 { return idgen.NextId() }
 
-func NextStringID() string { return strconv.FormatInt(idgen.NextId(), 10) }
+func NextYitterStringID() string { return strconv.FormatInt(idgen.NextId(), 10) }
 
 func NextUUID() string { return strings.ReplaceAll(uuid.NewString(), "-", "") }
 
